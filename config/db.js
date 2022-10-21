72b1016bb4bfd34b43924623fc99c5922831a9cf
Muhammad Zubairu <muhdzubairuahmad@gmail.com>
2022-10-21 21:37:03 +0100

    feat: frontend

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose')
 const DbConnection=async () => {
     try {
         const conn=await mongoose.connect(process.env.MONGO_URL)
-        // console.log(`Database connected on:${conn.connection.host}`)
+        console.log(`Database connected on:${conn.connection.host}`)
     } catch(error) {
         console.error(error)
     }
